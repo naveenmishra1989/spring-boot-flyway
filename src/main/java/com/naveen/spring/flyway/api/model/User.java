68b4814b5396da648527b4e377c72c31e46c8c40
Naveen Mishra <79073302+naveenmishra1989@users.noreply.github.com>
2023-08-24 16:30:47 +0530

    Add files via upload
    first commits

---
+++ src/main/java/com/naveen/spring/flyway/api/model/User.java
@@ -0,0 +1,22 @@
+package com.naveen.spring.flyway.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "REGISTRATION_USERS", schema = "SONU")
+public class User {
+	@Id
+	@GeneratedValue
+	private int id;
+	private String username;
+	private String first_name;
+	private String last_name;
+	private String email;
+	private String mobile;
+}
